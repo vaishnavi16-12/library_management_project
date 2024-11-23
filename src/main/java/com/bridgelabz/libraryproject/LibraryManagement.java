5e7cc2a5a4b7d76cb75fa694a6ff2cc796c9549e
vaishnavi16-12 <vaishnavic16502@gmail.com>
2024-11-23 14:34:24 +0530

    Welcome to Library management project

---
+++ src/main/java/com/bridgelabz/libraryproject/LibraryManagement.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.libraryproject;
+
+
+public class LibraryManagement {
+    public static void main(String[] args) {
+        System.out.println("Welcome to library management project ");
+
+    }
+}
