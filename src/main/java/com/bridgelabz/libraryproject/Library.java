9b0ca5a68874e5ddc3fc8f5c1450e08698b65d2d
vaishnavi16-12 <vaishnavic16502@gmail.com>
2024-11-25 12:31:03 +0530

    Added : search book in library

--- src/main/java/com/bridgelabz/libraryproject/Library.java
+++ src/main/java/com/bridgelabz/libraryproject/Library.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Library {
 
-    private  String name;
+    private String name;
     private String address;
     private List<Book> books;
     private List<Member> members;
@@ -15,23 +15,35 @@ public class Library {
         this.name = name;
         this.address = address;
         this.books = new ArrayList<>();
-        this.members =new ArrayList<>();
+        this.members = new ArrayList<>();
         this.librarians = new ArrayList<>();
     }
 
-    public  void addBook( Book book){
+    public void addBook(Book book) {
         books.add(book);
-        System.out.println("Book added: "+ book.getTitle());
-    }
-    public  void registerMember( Member member){
-        members.add(member);
-        System.out.println("Member Registrated : "+ member.getName());
-    }
-    public  void removeMember( Member member){
-        members.remove(member);
-        System.out.println("Member removed : "+ member.getName());
+        System.out.println("Book added: " + book.getTitle());
     }
 
+    public void registerMember(Member member) {
+        members.add(member);
+        System.out.println("Member Registrated : " + member.getName());
+    }
+
+    public void removeMember(Member member) {
+        members.remove(member);
+        System.out.println("Member removed : " + member.getName());
+    }
+
+    public List<Book> searchBook (String title) {
+     List<Book> result = new ArrayList<>();
+     for (Book book :books){
+         if(book.getTitle().equalsIgnoreCase(title)){
+             result.add(book);
+
+         }
+    }
+     return  result;
+}
 
 
 }
